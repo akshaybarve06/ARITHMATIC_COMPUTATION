74199efbfc2a76297f7906eb5027826001c5be9e
Admin <none@gmail.com>
2020-03-05 19:06:01 +0530

    Added: Take Three Inputs From User

---
+++ arithmaticComputationProblem.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+# @Description : Arithmatic Computation Problem
+# @Author : Akshay Dhananjay Barve
+# @Version : 18.04.3 lts
+# @Since : 5 March 2020 / Thursday
+
+read -p "Enter First Number" firstNumber
+read -p "Enter Second Number" secondNumber
+read -p "Enter Third Number" thirdNumber
