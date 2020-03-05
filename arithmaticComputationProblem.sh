c09b383099484d6249d7ba4b0ed4cb3b11b9d415
Akshay Barve <61134490+akshaybarve06@users.noreply.github.com>
2020-03-05 19:17:51 +0530

    Added: Take Three Inputs From User

--- arithmaticComputationProblem.sh
+++ arithmaticComputationProblem.sh
@@ -5,6 +5,6 @@
 # @Version : 18.04.3 lts
 # @Since : 5 March 2020 / Thursday
 
-read -p "Enter First Number" firstNumber
-read -p "Enter Second Number" secondNumber
-read -p "Enter Third Number" thirdNumber
+read -p "Enter First Number" numberOne
+read -p "Enter Second Number" numberTwo
+read -p "Enter Third Number" numberThree
