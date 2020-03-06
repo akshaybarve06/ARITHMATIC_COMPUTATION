832597ef7fecc81dffc01ee87ae3d747d4a8fbee
Admin <none@gmail.com>
2020-03-06 12:45:36 +0530

    Added: Sort Array In Ascending Order

--- arithmaticComputationProblem.sh
+++ arithmaticComputationProblem.sh
@@ -28,7 +28,7 @@ function findResult()
 	storeResult[2]=$secondEquationResult
 
 	# Expression c+a/b
-	thirdEquationResult=`expr "scale=2; $numberThree + $numberOne / $numberTwo "|bc`
+	thirdEquationResult=$(($numberThree + $numberOne / $numberTwo ))
 	storeResult[3]=$thirdEquationResult
 
 	# Expression a%b+c
@@ -43,6 +43,7 @@ function readValuesToArray()
 		arrayElements[index]=${storeResult[$index]}
 	done
 	descendingSort
+	ascendingSort
 }
 
 function descendingSort()
@@ -52,7 +53,7 @@ function descendingSort()
 	do
 		for ((j=i+1; j<${#arrayElements[@]}; j++ ))
 		do
-			if [[ ${arrayElements[i]} < ${arrayElements[j]} ]]
+			if [[ ${arrayElements[i]} -lt ${arrayElements[j]} ]]
 			then
 				temp=${arrayElements[i]}
 				arrayElements[i]=${arrayElements[j]}
@@ -62,5 +63,24 @@ function descendingSort()
 	done
 	echo Array In Descending Order..${arrayElements[@]}
 }
+
+function ascendingSort()
+{
+	temp=0
+	for ((i=0; i<${#arrayElements[@]}; i++ ))
+	do
+		for ((j=i+1; j<${#arrayElements[@]}; j++ ))
+		do
+			if [[ ${arrayElements[i]} -gt ${arrayElements[j]} ]]
+			then
+				temp=${arrayElements[i]}
+				arrayElements[i]=${arrayElements[j]}
+				arrayElements[j]=$temp
+			fi
+		done
+	done
+	echo Array In Ascending Order..${arrayElements[@]}
+}
+
 getInputs
 readValuesToArray
