57b965126d56cacf00f3b8cb4609ffc141d4bc06
Admin <none@gmail.com>
2020-03-06 11:57:58 +0530

    Added: Sort Array In Descending Order

--- arithmaticComputationProblem.sh
+++ arithmaticComputationProblem.sh
@@ -15,48 +15,52 @@ read -p "Enter First Number" numberOne
 read -p "Enter Second Number" numberTwo
 read -p "Enter Third Number" numberThree
 
-firstEquation
-secondEquation
-thirdEquation
-fourthEquation
+findResult
 }
 
-function firstEquation()
-{
-	# Expression a+b*c
+function findResult()
+{	# Expression a+b*c
 	firstEquationResult=$(( $numberOne + $numberTwo * $numberThree ))
 	storeResult[1]=$firstEquationResult
-}
 
-function secondEquation()
-{
 	# Expression a*b+c
 	secondEquationResult=$(( $numberOne * $numberTwo + $numberThree ))
 	storeResult[2]=$secondEquationResult
-}
 
-function thirdEquation()
-{
 	# Expression c+a/b
 	thirdEquationResult=`expr "scale=2; $numberThree + $numberOne / $numberTwo "|bc`
 	storeResult[3]=$thirdEquationResult
-}
 
-function fourthEquation()
-{
 	# Expression a%b+c
 	fourthEquationResult=$(($numberOne % $numberTwo + $numberThree ))
 	storeResult[4]=$fourthEquationResult
 }
 
-
 function readValuesToArray()
 {
-	for ((index=0; index<${#storeResult[@]}; index++ ))
+	for ((index=0; index<=${#storeResult[@]}; index++ ))
 	do
 		arrayElements[index]=${storeResult[$index]}
 	done
+	descendingSort
+}
+
+function descendingSort()
+{
+	temp=0
+	for ((i=0; i<${#arrayElements[@]}; i++ ))
+	do
+		for ((j=i+1; j<${#arrayElements[@]}; j++ ))
+		do
+			if [[ ${arrayElements[i]} < ${arrayElements[j]} ]]
+			then
+				temp=${arrayElements[i]}
+				arrayElements[i]=${arrayElements[j]}
+				arrayElements[j]=$temp
+			fi
+		done
+	done
+	echo Array In Descending Order..${arrayElements[@]}
 }
 getInputs
 readValuesToArray
-
